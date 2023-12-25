974930207f21d7a3c48e6485854941b6c65ea624
badrkacimi <badrvkacimi@gmail.com>
2023-12-25 02:33:32 +0100

    Initial commit

---
+++ src/test/java/com/stayUpdated/microservices/currencyexchangeservice/CurrencyExchangeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stayUpdated.microservices.currencyexchangeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
