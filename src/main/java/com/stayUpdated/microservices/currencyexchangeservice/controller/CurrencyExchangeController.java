66bb1a83b2ed689d75bcd515947f0b6cb30d615b
badrkacimi <badrvkacimi@gmail.com>
2023-12-25 21:59:30 +0100

    add Repo and controller

--- src/main/java/com/stayUpdated/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ src/main/java/com/stayUpdated/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -1,16 +1,30 @@
 package com.stayUpdated.microservices.currencyexchangeservice.controller;
 
 import com.stayUpdated.microservices.currencyexchangeservice.data.CurrencyExchange;
+import com.stayUpdated.microservices.currencyexchangeservice.repositories.CurrencyExchangeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
 public class CurrencyExchangeController {
 
-    @GetMapping("/currency-exchange/from/{from}/to/{to}")
-    public CurrencyExchange retrieveExchangeValue(@PathVariable String from,@PathVariable String to){
+    @Autowired
+    CurrencyExchangeRepository currencyExchangeRepository;
 
-        return null;
+    @Autowired
+    Environment environment;
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
+        CurrencyExchange currencyExchange = currencyExchangeRepository.findByFromAndTo(from, to)
+                .orElseThrow(() -> new RuntimeException("Unable to find data for: " + from + " to: " + to));
+
+        currencyExchange.setEnvironment(environment.getProperty("local.server.port"));
+        return currencyExchange;
     }
 }
