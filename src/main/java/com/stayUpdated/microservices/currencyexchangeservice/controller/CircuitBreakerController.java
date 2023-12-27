fb3dda33e1055a933112437dd3fe896b2fc9a6d0
badrkacimi <badrvkacimi@gmail.com>
2023-12-27 15:29:16 +0100

    add resilience4j as a circuit breaker

---
+++ src/main/java/com/stayUpdated/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -0,0 +1,27 @@
+package com.stayUpdated.microservices.currencyexchangeservice.controller;
+
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class CircuitBreakerController {
+
+    private final Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
+    @GetMapping("/api/circuitbreaker")
+    @Retry(name = "simpleApi", fallbackMethod = "fallbackResponse")
+    public String simpleApi(){
+        logger.info("Inside simpleApi");
+        ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/dummy-url", String.class);
+        return forEntity.getBody();
+    }
+
+    public String fallbackResponse(Exception ex){
+        logger.info("Inside fallbackResponse");
+        return "fallbackResponse";
+    }
+}
